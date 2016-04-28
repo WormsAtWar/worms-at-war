f5147f6eb479b85d2571f8478550fb952f930eaf
enseidler <enseidler@gmail.com>
2016-04-28 11:09:47 -0300

    More deploy heroku configuration

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ var waw = require('./game/worms-at-war');
 
 app.use(express.static(__dirname + '/public'));
 
-server.listen(3000, function() {
-	console.log("Socket listening on port 3000");
+server.listen(process.env.PORT || 3000, function() {
+	console.log("Socket listening on port %d ", this.address().port);
 });
 
 //////////////////////////////////////////////////////
