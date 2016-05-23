2a665b40466b9f7e77df2eb9b0329236fb30afc0
ezequiel <enseidler@gmail.com>
2016-05-23 01:11:19 -0300

    Collision with food fixed

--- model/food.js
+++ model/food.js
@@ -8,6 +8,6 @@ module.exports = function Food(id, x, y) {
 	this.x = x;
 	this.y = y;
 	this.points = availablePoints[Math.floor(Math.random() * availablePoints.length)];
-	this.boundary = new CircularBoundary(this.x, this.y, 5);
+	this.boundary = new CircularBoundary(this.x, this.y, this.points / 2);
 	
 }
